8f7b72df68e3c337d1f17ba044a8a8210c14028f
Karthikeyan49 <karthikeyansenthilkumar0@gmail.com>
2026-04-13 02:35:30 +0530

    logo

--- components/feature/Logo.tsx
+++ components/feature/Logo.tsx
@@ -7,13 +7,13 @@ interface LogoProps {
 }
 
 export function Logo({ size = 'md' }: LogoProps) {
-  const width = size === 'sm' ? 80 : size === 'lg' ? 140 : 110;
+  const width = size === 'sm' ? 80 : size === 'lg' ? 150 : 130;
   const height = size === 'sm' ? 40 : size === 'lg' ? 70 : 55;
 
   return (
     <View style={styles.container}>
-      <Image 
-        source={require('@/assets/images/logo.png')} 
+      <Image
+        source={require('@/assets/images/logo.png')}
         style={{ width, height }}
         contentFit="contain"
       />
